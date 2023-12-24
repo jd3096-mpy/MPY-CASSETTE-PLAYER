6cc66f1d94d522848eac9776567b2d0d5d02c1a7
jd3096-mpy <1091329318@qq.com>
2023-12-24 21:43:53 +0800

    boot mode

--- src/bootloader.py
+++
@@ -1,2 +0,0 @@
-import machine
-machine.bootloader(True)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+from machine import Pin
+b=Pin(4,Pin.IN,Pin.PULL_UP)
+if b.value()==0:
+    import machine
+    machine.bootloader(True)
+
 import gc
 gc.enable()
 import uasyncio as asyncio
